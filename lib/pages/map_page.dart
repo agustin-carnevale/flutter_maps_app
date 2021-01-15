fe4394e806726c4e71e36787c88f3a313330b4dc
Agustin Carnevale <acarnevale@ecointeractive.com>
2021-01-15 02:22:17 -0300

    small change

--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+
 import 'package:maps_app/bloc/map/map_bloc.dart';
 import 'package:maps_app/bloc/my_location/my_location_bloc.dart';
 import 'package:maps_app/widgets/widgets.dart';
